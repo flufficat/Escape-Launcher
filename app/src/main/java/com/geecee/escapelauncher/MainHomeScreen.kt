b017ebb9e0b1b13de2c1478177ee9822295e6385
George Clensy <george.clensy@gmail.com>
2025-04-13 19:10:40 +0100

    Only satus bars are hidden now for smoother experience with back and recents

--- app/src/main/java/com/geecee/escapelauncher/MainHomeScreen.kt
+++ app/src/main/java/com/geecee/escapelauncher/MainHomeScreen.kt
@@ -296,14 +296,14 @@ class MainHomeScreen : ComponentActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
             window.insetsController?.apply {
-                hide(WindowInsets.Type.systemBars())
+                hide(WindowInsets.Type.statusBars()) // Only hide the status bar
                 systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         } else {
             window.decorView.systemUiVisibility = (
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or // Only hide the status bar
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     )
             window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }
